c34ee7e5e2230f1796f386a6bca37d49945cc015
lisider <1368317399@qq.com>
2021-03-04 11:13:22 +0800

    [添加配置编译脚本]

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+[ -f log_build ] && rm log_build
+make O=output CROSS_COMPILE=arm-linux-gnueabi- V=1 2>&1 | tee -a  log_build
+#arm-linux-gnueabi-gcc (Linaro GCC 7.4-2019.02) 7.4.1 20181213 [linaro-7.4-2019.02 revision 56ec6f6b99cc167ff0c2f8e1a2eed33b1edc85d4]
+#Copyright (C) 2017 Free Software Foundation, Inc.
+#This is free software; see the source for copying conditions.  There is NO
+#warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
---
+++ config.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+make O=output ok6410a_defconfig
